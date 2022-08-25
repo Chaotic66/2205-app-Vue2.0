d1c7ea9065d5390973e48eea4b4b1b20a971f7bf
Chaotic66 <781452701@qq.com>
2022-08-25 21:56:22 +0800

    账密登录+微信登录完成

--- src/pages/login/commonLogin.vue
+++ src/pages/login/commonLogin.vue
@@ -1,15 +1,20 @@
 <template>
     <div class="commonlogin" >
         <el-form :model="ruleForm" status-icon :rules="rules" ref="ruleForm" label-width="100px" class="demo-ruleForm">
+            <!-- //!  用户名-->
             <el-form-item class="userSec" label="用户名" prop="username">
                 <el-input type="text" v-model="ruleForm.username" autocomplete="off"></el-input>
             </el-form-item>
+            <!-- //!  密码-->
             <el-form-item label="密码" prop="password">
                 <el-input type="password" v-model="ruleForm.password" autocomplete="off"></el-input>
             </el-form-item>
+            <!-- //!  验证码-->
             <el-form-item label="验证码" prop="captcha">
                 <el-input type="text" v-model.number="ruleForm.captcha"></el-input>
+                <i  alt="" class="imgYz" v-html=captchaImg @click=getCapchaM></i>    
             </el-form-item>
+            <!-- //!  提交-->
             <el-form-item>
                 <el-button  type="primary" @click="submitForm('ruleForm')">提交</el-button>
             </el-form-item>
@@ -48,15 +53,19 @@
       };
     ////验证 验证码
     var vvalidateCaptcha = (rule, value, callback) => {
-        if(value){
+        if(value.length > 4){
             callback()
         }else{
             callback('不会输？')
         }
 
     };
+
+    import imgSrc from '@/assets/img/yzm.png'
+    import * as api from '@/api/users'
+    import storage from '@/utils/storage'
     export default {
-        props:['mobile'],
+        
         data() {
             return {
                 ruleForm: {
@@ -75,29 +84,69 @@
                         { validator: vvalidateCaptcha, trigger: 'blur' }
                     ],
                 },
+                imgSrc2:imgSrc,
+                captchaImg:''
 
             };
         },
         methods:{
-            changeMobile(){
-                ////切换短信登录
-                this.$emit('custom','mobileLogin')
-            },
             submitForm(formName) {
                 this.$refs[formName].validate((valid) => {
                 if (valid) {
-                    alert('submit!');
+                    ////发起校验 验证码请求
+                    api.verifyCapcha(this.ruleForm.captcha)
+                    .then(res=>{                        
+                        if(res.data.state){
+                            //!若本地校验通过，则发起远程校验请求
+                            console.log('启动！');
+                            api.normalLogin(this.ruleForm.username,this.ruleForm.password)
+                            .then(res=>{
+                                
+                                //!远程校验通过，存数据
+                                storage.set('token',res.data.token)
+                                storage.set('userInfo',res.data.userInfo)
+                                storage.set('permission',res.data.permission)
+                                //!远程校验通过，跳转
+                                this.$router.push('/home')
+
+                            })
+                        }else{
+                            ////刷新验证码
+                            this.getCapchaM()                        
+                        }
+                    })
+                    
+
                 } else {
                     console.log('error submit!!');
                     return false;
                     
                 }
                 });
+
             },
             resetForm(formName) {
                 this.$refs[formName].resetFields();
             },
+            ////切换短信登录
+            changeMobile(){
+                this.$emit('custom','mobileLogin')
+            },
+            ////获取验证码
+            getCapchaM(){
+                api.getCapcha()
+                .then(res=>{
+                    this.captchaImg = res.data.img
+                    // console.log(res);
+                    
+                })
+            },
+           
         },
+        mounted(){
+            // console.log('111');
+            this.getCapchaM()
+        }
        
     }
 
@@ -140,6 +189,14 @@
         ////用户名文字
         /deep/.el-form-item__label{
                 color: white !important;
+        }
+
+        ////验证码图片
+        /deep/.imgYz svg{
+            height: 40px;
+            position: absolute;
+            right: 40px;
+            top: 1px;
         }
     }
 
